7e8b6dfc151ede34454faa2090c916ef32190f66
ABWassim <wassim.belkacem99@gmail.com>
2025-08-14 14:31:04 +0200

    feat(overview): sorting + loading + filtering

--- src/app/overview/overview.ts
+++ src/app/overview/overview.ts
@@ -1,51 +1,77 @@
-import { Component, inject, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { HttpService } from './../http-service';
 import { SimpleVulnerabilityReport } from '../../dto/main';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { SimpleVulnerabilityReportModel } from '../../model/main';
 import { CommonModule } from '@angular/common';
 import { RouterLink} from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { map } from 'rxjs';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
+  standalone: true,
   selector: 'app-overview',
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule, RouterLink],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule, RouterLink, MatProgressSpinnerModule, FormsModule, MatInputModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './overview.html',
   styleUrl: './overview.scss'
 })
 
-export class Overview {
-  protected title = 'trivy-operator-web-ui';
-
+export class Overview implements OnInit, AfterViewInit {
   private readonly httpService = inject(HttpService);
 
+  protected reports: SimpleVulnerabilityReportModel[] = [];
   protected dataSource = new MatTableDataSource<SimpleVulnerabilityReportModel>();
-  protected displayedColumns: string[] = ['name', 'criticalCount', 'highCount', 'mediumCount', 'lowCount', 'noneCount', 'unknownCount'];
+  protected displayedColumns: string[] = ['image', 'criticalCount', 'highCount', 'mediumCount', 'lowCount', 'noneCount', 'unknownCount'];
+  protected namespaces = new Set<string>();
+  public reportsLoading = true;
+
+  public imageFilter = '';
+  public namespace = '';
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
-    this.httpService.getVulnerabilityReports().subscribe( _reports => {
-      let modelReports: SimpleVulnerabilityReportModel[] = [];
+    this.httpService.getVulnerabilityReports().pipe(
+      map(_reports => {
+          return _reports.map(r => {
+            this.namespaces.add(r.namespace);
+            return this.mapDtoToModel(r);
+          })
+        }
+      )
+    ).subscribe(_reports => {
+      this.reports = _reports;
+      this.namespace = localStorage.getItem('namespace') ?? this.reports[0].namespace;
+      this.dataSource.data = this.reports.filter(report => report.namespace === this.namespace);
 
-      for (const report of _reports) {
-        modelReports.push(this.mapDtoToModel(report));
-      }
+      this.imageFilter = localStorage.getItem('imageFilter') ?? '';
 
-      this.dataSource.data = modelReports;
+      this.reportsLoading = false;
     })
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.dataSource.filterPredicate = function (data, filter) {
+      // Maybe Jaro-Winkler in the future ?
+      return data.image.includes(filter)
+    }
+    this.dataSource.filter = localStorage.getItem('imageFilter') ?? '';
+
+    const sort: Sort = JSON.parse(localStorage.getItem('overviewSort') || '{}');
+    if (sort.direction) this.sort.sort({id: sort.active, start: sort.direction, disableClear: false});
   }
 
   private mapDtoToModel(report: SimpleVulnerabilityReport): SimpleVulnerabilityReportModel {
     return {
-      name: report.name,
       uid: report.uid,
       criticalCount: report.summary.criticalCount,
       highCount: report.summary.highCount,
@@ -53,10 +79,24 @@ export class Overview {
       lowCount: report.summary.lowCount,
       noneCount: report.summary.noneCount,
       unknownCount: report.summary.unknownCount,
-      digest: report.artifact.digest,
-      mimeType: report.artifact.mimeType,
-      repository: report.artifact.repository,
-      tag: report.artifact.tag,
+      namespace: report.namespace,
+      image: report.image,
     }
   }
+
+  protected saveSortDirection(sortState: Sort) {
+    localStorage.setItem('overviewSort', JSON.stringify(sortState));
+  }
+
+  protected filterNamespace(event: MatSelectChange<string>) {
+    const namespace = event.value;
+    this.dataSource.data = this.reports.filter(report => report.namespace === namespace);
+    localStorage.setItem('namespace', namespace);
+  }
+
+  protected filterTable(event: KeyboardEvent) {
+    const imageFilter = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = imageFilter.trim().toLowerCase();
+    localStorage.setItem('imageFilter', imageFilter);
+  } 
 }
