969f8d3e05f643dea67cd1d18d52d4a1e687734e
ABWassim <wassim.belkacem99@gmail.com>
2025-08-14 14:28:12 +0200

    chore: changed severity to be a number
    Easier to implement sorting on future mat-table

---
+++ src/app/severity-service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Severity } from '../dto/main';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeverityService {
+ 
+  private severityToScore = ['UNKNOWN', 'LOW', 'MEDIUM', 'HIGH', 'CRITICAL'];
+
+  public toString(severity: Severity): string {
+    return this.severityToScore[severity];
+  }
+}
