d70dbeef20184f77df919739d4c8c61ebc689258
ABWassim <wassim.belkacem99@gmail.com>
2025-08-14 14:31:23 +0200

    feat(detailed): finished + sorting + loading

--- src/app/detailed-report/detailed-report.ts
+++ src/app/detailed-report/detailed-report.ts
@@ -3,14 +3,17 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpService } from '../http-service';
 import { Vulnerability, VulnerabilityReport } from '../../dto/main';
 import { MatCardModule } from '@angular/material/card';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LowerCasePipe } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SeverityService } from '../severity-service';
 
 @Component({
   standalone: true,
   selector: 'app-detailed-report',
-  imports: [MatCardModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [MatCardModule, MatTableModule, MatPaginatorModule, MatSortModule, LowerCasePipe, MatProgressSpinnerModule],
   templateUrl: './detailed-report.html',
   styleUrl: './detailed-report.scss'
 })
@@ -19,6 +22,7 @@ export class DetailedReport implements AfterViewInit {
   
   private readonly route: ActivatedRoute = inject(ActivatedRoute);
   private readonly httpService = inject(HttpService);
+  protected readonly severityService = inject(SeverityService);
 
   private readonly uid = this.route.snapshot.params['uid']
   protected vulnReport: VulnerabilityReport | undefined;
@@ -34,9 +38,19 @@ export class DetailedReport implements AfterViewInit {
       this.dataSource.data = _report.report.vulnerabilities
     })
   }
+
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator
+    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.restoreSortDirection();
   }
 
+  protected saveSortDirection(sortState: Sort) {
+    localStorage.setItem('detailedSort', JSON.stringify(sortState));
+  }
+  
+  private restoreSortDirection() {
+    const sort: Sort = JSON.parse(localStorage.getItem('detailedSort') || '{}');
+    if (sort.direction) this.sort.sort({id: sort.active, start: sort.direction, disableClear: false});
+  }
 }
